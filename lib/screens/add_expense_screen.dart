40f9721e4d788816d98bfe32785670bec7dbbe4c
herotux <thefreetux@gmail.com>
2025-07-31 20:19:16 +0330

    Add person and personId fields to Expense model for API compatibility

--- lib/screens/add_expense_screen.dart
+++ lib/screens/add_expense_screen.dart
@@ -48,7 +48,7 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
         amount: _amount!.toInt(),
         date: _selectedDate.toIso8601String(),
         category: _selectedCategoryId,
-        person: _selectedPerson?.id,
+        person: _selectedPersonId,
         tag: _selectedTagId,
       );
 
