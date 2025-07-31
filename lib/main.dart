987fa8d00de44347658ec98616729ce52a482958
herotux <thefreetux@gmail.com>
2025-07-31 16:27:19 +0330

    Add '/edit-expense' route for navigating to expense edit screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ import 'package:daric/screens/add_debt_screen.dart';
 import 'package:daric/screens/settings_screen.dart';
 import 'package:daric/screens/add_income_screen.dart';
 import 'package:daric/screens/add_expense_screen.dart';
+import 'package:daric/screens/edit_expense_screen.dart';
 import 'package:daric/screens/log_screen.dart';
 import 'package:daric/screens/persons_screen.dart';
 
