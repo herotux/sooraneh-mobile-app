8605e20253abb0ef11d49ae45c6f0761f9cb95fe
herotux <thefreetux@gmail.com>
2025-07-31 16:53:43 +0330

    fix ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,6 +16,8 @@ android {
         versionName = flutter.versionName
     }
 
+    ndkVersion = "27.0.12077973"
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
