40dbb0b13cfa89dd32689483863067d459a2eb60
herotux <thefreetux@gmail.com>
2025-08-09 16:25:24 +0330

    fix android

--- android/app/src/main/kotlin/com/example/daric/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/daric/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.daric_new
+package com.example.daric
 
 import io.flutter.embedding.android.FlutterActivity
 
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -12,13 +12,13 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal() // Recommended for general Gradle plugins
+        gradlePluginPortal()
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.12.0" apply false
+    id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
